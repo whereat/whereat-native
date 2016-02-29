f3a8456da7524bde20c3bec61d5bb3b64c7d3438
Brad Menchl <brmenchl@gmail.com>
2016-02-29 13:36:50 -0500

    BM - Bring locationReducer up to date

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -2,28 +2,51 @@ import React, {
   Component,
   StyleSheet,
   Text,
-  View
+  View,
+  DeviceEventEmitter,
+  NativeModules
 } from 'react-native';
+
 import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
 
 import {locationChanged} from '../redux/modules/location';
 
+const LOSTLocationProvider = NativeModules.LOSTLocationProvider;
 import MapView from '../components/MapView';
 
-export default class App extends Component {
-  render() {
-    return (
-      <MapView
-        style={styles.map}
-        zoom={10}
-        center={{
-          lat: 41.8369,
-          lon: -87.6847
-        }}
-      />
-    );
-  }
+class App extends Component {
+	componentWillMount() {
+		LOSTLocationProvider.startLocationPolling(500, 0.1, LOSTLocationProvider.HIGH_ACCURACY);
+
+		this.locationChangedListener = DeviceEventEmitter.addListener(
+			'location_changed',
+			(location) => this.props.locationChanged({
+				...location,
+				lastUpdatedTime: new Date().getTime()
+			})
+		);
+	}
+
+	render() {
+		return (
+      <View style={styles.container}>
+        <View>
+          <Text>{`LATITUDE: ${this.props.location.latitude}\n`}</Text>
+          <Text>{`LONGITUDE: ${this.props.location.longitude}\n`}</Text>
+          <Text>{`LAST UPDATED TIME: ${this.props.location.lastUpdatedTime}`}</Text>
+        </View>
+        <MapView
+          style={styles.map}
+          zoom={10}
+          center={{
+            lat: 41.8369,
+            lon: -87.6847
+          }}
+        />
+      </View>
+		);
+	}
 }
 
 function mapStateToProps(state) {
@@ -33,12 +56,15 @@ function mapStateToProps(state) {
 }
 
 function mapDispatchToProps(dispatch) {
-  return {
-    locationChanged: bindActionCreators({locationChanged}, dispatch)
-  };
+	return {
+		locationChanged: bindActionCreators(locationChanged, dispatch)
+	};
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
   map: {
     flex: 1
   }
