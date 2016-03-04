e409a971685ee8ef39e981c30b03c31265b9da39
Timothy <timothyakampa@gmail.com>
2016-03-04 15:28:42 -0500

    [M#169] - [TA/JV] - Adds functional tests for showing current location. Includes some refactoring.

--- tests/functional/App.spec.js
+++ tests/functional/App.spec.js
@@ -1,26 +1,25 @@
 import {
     wd,
     expect,
-    should
+    should,
+    path
 } from "./setup"
 
 describe("App", () => {
-
     var driver;
 
     const androidCapabilities = {
+        app:path.resolve(__dirname, "../../android/app/build/outputs/apk/app-debug.apk"),
         browserName: '',
         'appium-version': '1.5.0',
         platformName: 'Android',
         platformVersion: '5.1.0',
-        deviceName: process.env.ANDROID_EMULATOR,
-        appActivity: '.MainActivity',
-        appPackage: 'io.whereat.mobile'
+        deviceName: "Android Emulator"
     };
 
     before(() => {
         driver = wd.promiseChainRemote({ host: 'localhost', port: 4723 });
-        return driver.init(androidCapabilities).setImplicitWaitTimeout(2000);
+        return driver.init(androidCapabilities).setImplicitWaitTimeout(8000);
     });
 
     after(() => {
@@ -29,8 +28,14 @@ describe("App", () => {
 
     it("should have user's current location", () => {
         return driver
-            .elementByXPath('//android.widget.LinearLayout[1]/android.widget.FrameLayout[1]/android.widget.FrameLayout[1]/android.widget.TextView[1]')
-            .should.eventually.exist;
+            .elementByXPath('//android.widget.TextView[1]')
+            .text().should.become('LATITUDE: 42.64565\n')
+
+            .elementByXPath('//android.widget.TextView[2]')
+            .text().should.become('LONGITUDE: -73.754199\n')
+
+            .elementByXPath('//android.widget.TextView[3]')
+            .text().should.become('LAST UPDATED TIME: 1455227589')
     });
 });
 
--- tests/functional/setup.js
+++ tests/functional/setup.js
@@ -1,5 +1,5 @@
 var wd = require("wd");
-
+var path = require("path");
 var chai = require("chai");
 var chaiAsPromised = require("chai-as-promised");
 chai.use(chaiAsPromised);
@@ -9,4 +9,5 @@ chaiAsPromised.transferPromiseness = wd.transferPromiseness;
 
 exports.should = should;
 exports.expect = expect;
+exports.path = path;
 exports.wd = wd;
