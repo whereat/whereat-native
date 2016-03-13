d5d032d6d7616177f7833de5560ce22a419e7d7a
aguestuser <aguestuser@riseup.net>
2016-03-13 16:56:44 -0400

    [#M122] AG | Remove jest directory

--- jest/setupTestFramework.js
+++
@@ -1,9 +0,0 @@
-/**
-  This file is for any setup functionality before any tests are executed
-  README: https://facebook.github.io/jest/docs/api.html#config-setuptestframeworkscriptfile-string
-*/
-
-// Turn auto-mocking off. We'll mock everything out purposefully for the sake of increased determinism and clarity
-jest.autoMockOff();
-
-  const should = require('chai').should();
