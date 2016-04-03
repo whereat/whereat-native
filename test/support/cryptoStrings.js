2c180053072c1d9ebd7b90a6ad6b104a380fb400
aguestuser <aguestuser@riseup.net>
2016-04-02 21:25:22 -0400

    AG | Add GPL notice to all files
    To ensure compliance with terms of GPL, add copyright & license notice
    to top of every source file.

--- test/support/cryptoStrings.js
+++ test/support/cryptoStrings.js
@@ -1,1 +1,18 @@
+/**
+ *
+ * Copyright (c) 2016-present, Total Location Test Paragraph.
+ * All rights reserved.
+ *
+ * This file is part of Where@. Where@ is free software:
+ * you can redistribute it and/or modify it under the terms of
+ * the GNU General Public License (GPL), either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * Where@ is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. For more details,
+ * see the full license at <http://www.gnu.org/licenses/gpl-3.0.en.html>
+ *
+ */
+
 export const zucottiHmac = '0c79bc45c9d21ddc8a8766bc08069fcd613a0527a05b7b90f7e1b378f27cfab4';
